855eb5003081a44990b477e0bf10438d610b5ff2
Leul Tefera <leulmulugeta1@gmail.com>
2022-08-16 21:27:12 -0500

    Initial commit for Navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,65 @@
+import './Navbar.css';
+
+import React, { useState, useRef } from 'react';
+
+function Navbar() {
+  return (
+    <Nav>
+      <Items>
+        <Dropdown></Dropdown>
+      </Items>
+    </Nav>
+  );
+}
+
+function Nav(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+}
+
+
+// Items to be inserted inside the dropdown menu
+function Items(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  );
+}
+
+
+
+// Dynamically inserted dropdown menu
+
+function Dropdown() {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const dropdownRef = useRef(null);
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+        {props.children}
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown" ref={dropdownRef}>
+      <div className="menu">
+        <h3 style={{ color: "white" }}>Leul Tefera</h3>
+        <DropdownItem>Dashboard</DropdownItem>
+        <DropdownItem>Sign Out</DropdownItem>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
